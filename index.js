58510cda3f41ba38861ce1f1193bdadc5b4e5268
Venryx <venryx@gmail.com>
2016-12-25 23:49:06 -0800

    * Added reconnectAttemptInterval setting. (which is really helpful for keeping device connected while asleep or the like)

--- index.js
+++ index.js
@@ -7,22 +7,53 @@ Object.defineProperty(exports, "__esModule", {value: true});
 var {NativeModules, DeviceEventEmitter} = require("react-native");
 var LibMuse = NativeModules.LibMuse;
 
-var listeners_onReceiveMuseDataPacket = [];
-var listeners_onChangeMuseConnectStatus = []
-var listeners_onChangeMuseList = [];
-//module.exports = {
-exports.default = {
-	Init: function () {
+class LibMuseJS {
+	listeners_onReceiveMuseDataPacket = [];
+	listeners_onChangeMuseConnectStatus = []
+	listeners_onChangeMuseList = [];
+
+	// set to -1, to disable reconnect-attempt helper
+	// (reconnection will still be attempted, by base library, but not
+	//		our disconnect-then-reconnect operation which helps increases the chances)
+	reconnectAttemptInterval = 10;
+
+	reconnectAttemptTimerID = -1;
+	StartReconnectAttemptTimer() {
+		// ensure we don't have two attempt-timers running at once
+		this.StopReconnectAttemptTimer();
+		// start timer
+		this.reconnectAttemptTimerID = setInterval(()=> {
+			// ensure still disconnected (it might be in the process of connecting)
+			if (this.connectionStatus == "disconnected") 
+				this.RestartSearch();
+		}, this.reconnectAttemptInterval);
+	}
+	StopReconnectAttemptTimer() {
+		if (this.reconnectAttemptTimerID == -1) return;
+		// stop timer
+		clearInterval(this.reconnectAttemptTimerID);
+		this.reconnectAttemptTimerID = -1;
+	}
+
+	connectionStatus = "disconnected";
+
+	Init() {
 		LibMuse.Init();
 		DeviceEventEmitter.addListener("OnChangeMuseList", args=> {
 			var [museList] = args;
-			for (let listener of listeners_onChangeMuseList)
+			for (let listener of this.listeners_onChangeMuseList)
 				listener(museList);
 		});
 		DeviceEventEmitter.addListener("OnChangeMuseConnectStatus", args=> {
 			var [status] = args;
-			for (let listener of listeners_onChangeMuseConnectStatus)
+			for (let listener of this.listeners_onChangeMuseConnectStatus)
 				listener(status);
+
+			this.connectionStatus = status;
+			if (status == "connected")
+				this.StopReconnectAttemptTimer();
+			else if (status == "disconnected" && this.reconnectAttemptInterval != -1)
+				this.StartReconnectAttemptTimer();
 		});
 		/*DeviceEventEmitter.addListener("OnReceiveMuseDataPacket", args=> {
 			var [type, channelValues] = args;
@@ -32,42 +63,44 @@ exports.default = {
 		DeviceEventEmitter.addListener("OnReceiveMuseDataPacketSet", args=> {
 			var [packets] = args;
 			for (let packet of packets) {
-				for (let listener of listeners_onReceiveMuseDataPacket)
+				for (let listener of this.listeners_onReceiveMuseDataPacket)
 					listener(packet);
 			}
 		});
-	},
+	}
 	
 	// searching
-	StopSearch: function() {
+	StopSearch() {
 		LibMuse.StopSearch();
-	},
-	StartSearch: function() {
+	}
+	StartSearch(reconnectAttemptInterval) {
 		LibMuse.StartSearch();
-	},
-	RestartSearch: function() {
+	}
+	RestartSearch(reconnectAttemptInterval) {
 		LibMuse.RestartSearch();
-	},
-	AddListener_OnChangeMuseList: function(listenerFunc) {
-		listeners_onChangeMuseList.push(listenerFunc);
-	},
+	}
+	AddListener_OnChangeMuseList(listenerFunc) {
+		this.listeners_onChangeMuseList.push(listenerFunc);
+	}
 	
 	// connecting
-	Connect: function(museIndex = 0) {
+	Connect(museIndex = 0) {
 		LibMuse.Connect(museIndex);
-	},
-	Disconnect: function () {
+	}
+	Disconnect() {
 		LibMuse.Disconnect();
-	},
-	AddListener_OnChangeMuseConnectStatus: function(listenerFunc) {
-		listeners_onChangeMuseConnectStatus.push(listenerFunc);
-	},
+	}
+	AddListener_OnChangeMuseConnectStatus(listenerFunc) {
+		this.listeners_onChangeMuseConnectStatus.push(listenerFunc);
+	}
 	
 	// data-streaming
-	TogglePaused: function() {
+	TogglePaused() {
 		LibMuse.TogglePaused();
-	},
-	AddListener_OnReceiveMuseDataPacket: function(listenerFunc) {
-		listeners_onReceiveMuseDataPacket.push(listenerFunc);
-	},
-};
+	}
+	AddListener_OnReceiveMuseDataPacket(listenerFunc) {
+		this.listeners_onReceiveMuseDataPacket.push(listenerFunc);
+	}
+}
+//module.exports = {
+exports.default = new LibMuseJS();
